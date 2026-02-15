281826151382c115a71a76221e9aa51b55409727
jayprakashm578 <jayprakashm578@gmail.com>
2026-02-16 00:54:27 +0530

    Fixed Netlify Spotify clone

--- javascript/script.js
+++ javascript/script.js
@@ -22,40 +22,34 @@ function secondsToMinutes(seconds) {
 
 async function getSongs(folder) {
     currFolder = folder;
-    let a = await fetch(`/${folder}/`)
-    let response = await a.text();
 
-    let div = document.createElement("div");
-    div.innerHTML = response;
-    let as = div.getElementsByTagName("a")
-    songs = []
+    let response = await fetch(`/${folder}/info.json`);
+    let data = await response.json();
 
-    for (let element of as) {
-        if (element.innerText.endsWith(".mp3")) {
-            songs.push(element.innerText.trim());
-        }
-    }
+    songs = data.songs;
 
-    // Add all the songs to playlist
-    let songUL = document.querySelector(".songlist").getElementsByTagName("ul")[0]
-    songUL.innerHTML = ""
+    let songUL = document.querySelector(".songlist ul");
+    songUL.innerHTML = "";
+
     songs.forEach((song, index) => {
         songUL.insertAdjacentHTML("beforeend",
             `<li data-index="${index}">
-            <img class="invert" src="/img/music.svg" alt="">
-            <div class="info">
-                <div>${song}</div>
-                <div>Song Artist</div>
-            </div>
-            <div class="playnow">
-                <span>Play Now</span>
-                <img class="invert" src="/img/play.svg" alt="">
-            </div>
-        </li>`
+                <img class="invert" src="/img/music.svg">
+                <div class="info">
+                    <div>${song}</div>
+                    <div>${data.title}</div>
+                </div>
+                <div class="playnow">
+                    <span>Play Now</span>
+                    <img class="invert" src="/img/play.svg">
+                </div>
+            </li>`
         );
     });
+
     return songs;
 }
+
 
 const playMusic = (track, pause = false) => {
 
@@ -80,54 +74,49 @@ const playMusic = (track, pause = false) => {
 };
 
 async function displayAlbums() {
-    let a = await fetch(`/songs/`)
-    let response = await a.text();
 
-    let div = document.createElement("div");
-    div.innerHTML = response;
+    let folders = ["ncs"];   // add more folders here later
 
-    let anchors = div.getElementsByTagName("a");
-    let cardContainer = document.querySelector(".cardContainer")
-    let array = Array.from(anchors)
-    for (let index = 0; index < array.length; index++) {
-        const e = array[index];
+    let cardContainer = document.querySelector(".cardContainer");
+    cardContainer.innerHTML = "";
 
-        if (e.href.includes("songs")) {
-            let folder = e.innerText.trim().slice(0, -1);
+    for (let folder of folders) {
 
-            //    Get the metadata of the folder
-            let a = await fetch(`/${folder}/info.json`)
-            let response = await a.json();
+        let res = await fetch(`/songs/${folder}/info.json`);
+        let data = await res.json();
 
-            cardContainer.innerHTML = cardContainer.innerHTML + `<div data-folder="${folder}" class="card">
-                        <div class="play">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"
-                                color="currentColor" fill="black" stroke="#141B34" stroke-width="1.5"
-                                stroke-linejoin="round">
-                                <path
-                                    d="M18.8906 12.846C18.5371 14.189 16.8667 15.138 13.5257 17.0361C10.296 18.8709 8.6812 19.7884 7.37983 19.4196C6.8418 19.2671 6.35159 18.9776 5.95624 18.5787C5 17.6139 5 15.7426 5 12C5 8.2574 5 6.3861 5.95624 5.42132C6.35159 5.02245 6.8418 4.73288 7.37983 4.58042C8.6812 4.21165 10.296 5.12907 13.5257 6.96393C16.8667 8.86197 18.5371 9.811 18.8906 11.154C19.0365 11.7084 19.0365 12.2916 18.8906 12.846Z" />
-                            </svg>
-                        </div>
-                        <img src="/songs/${folder}/cover.jpg" alt="">
-                        <h2>${response.title}</h2>
-                        <p>${response.description}</p>
-                    </div>`
-        }
+        cardContainer.innerHTML += `
+            <div data-folder="${folder}" class="card">
+                <div class="play">▶</div>
+                <img src="/songs/${folder}/cover.jpg">
+                <h2>${data.title}</h2>
+                <p>${data.description}</p>
+            </div>`;
     }
 
-    // Load the playlist when the card is clicked
-    Array.from(document.getElementsByClassName("card")).forEach(e => {
-        e.addEventListener("click", async item => {
-            songs = await getSongs(`songs/${item.currentTarget.dataset.folder}`);
+    // click event
+    Array.from(document.getElementsByClassName("card")).forEach(card => {
+
+        card.addEventListener("click", async (e) => {
+
+            let folder = e.currentTarget.dataset.folder;
+
+            songs = await getSongs(`songs/${folder}`);
+
             playMusic(songs[0]);
-        })
-    })
+
+        });
+
+    });
+
 }
+
 
 async function main() {
 
     // Get all the songs
-    let savedFolder = localStorage.getItem("currentFolder") || "songs/trending";
+    let savedFolder = localStorage.getItem("currentFolder") || "songs/ncs";
+
     await getSongs(savedFolder);
 
     // Display  all the albums on the page
